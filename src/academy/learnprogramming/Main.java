77bf3efbff30f499ab0bccd7c60f7bfd27b0bfd1
Preeti Rawat <preetirawat711@gmail.com>
2021-04-22 13:20:15 +0530

    Question1

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,6 +3,17 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	SimpleCalculator calculate=new SimpleCalculator();
+	calculate.setFirstNumber(5.0);
+	calculate.setSecondNumber(4);
+
+	System.out.println("Addition is: " + calculate.getAdditionResult());
+        System.out.println("Subtraction is: " + calculate.getSubtractionResult());
+
+        calculate.setFirstNumber(5.25);
+        calculate.setSecondNumber(0);
+
+        System.out.println("Multipication is: " + calculate.getMultiplicationResult());
+        System.out.println("Division is: " + calculate.getDivisionResult());
     }
 }
