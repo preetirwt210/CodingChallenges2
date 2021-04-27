1bf909777159c16722a55e7fde5a0bc71484b1eb
Preeti Rawat <preetirawat711@gmail.com>
2021-04-27 17:54:40 +0530

    Question9

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -113,5 +113,20 @@ public class Main {
     pagesPrinted= printer.printPages(2);
         System.out.println("pages printed was= "+ pagesPrinted +
          "new total print count for printer: "+ printer.getPagesPrinted()); */
+
+	Hamburger hamburger=new Hamburger("Basic", "Sausage", 3.56,"White");
+	hamburger.addHamburgerAddition1("Tomato",0.27);
+	hamburger.addHamburgerAddition2("Lettuce",0.75);
+	hamburger.addHamburgerAddition3("Cheese",1.13);
+	System.out.println("Total Burger price is: "+ hamburger.itemizeHamburger());
+
+	HealthyBurger healthyBurger=new HealthyBurger("Bacon",5.67);
+	healthyBurger.addHealthyAddition1("Egg",5.43);
+	healthyBurger.addHealthyAddition1("Lentils",3.41);
+	System.out.println("Total Healthy Burger price is: "+ healthyBurger.itemizeHamburger());
+
+	DeluxeBurger db=new DeluxeBurger();
+	db.addHamburgerAddition3("Should not do this", 50.53);
+	System.out.println("Total Deluxe Burger price is: "+ db.itemizeHamburger());
     }
 }
